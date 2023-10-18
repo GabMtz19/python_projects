5a03aac40665795fc638dd4dfc7507710ca2c602
Gabriel Martinez <gabjr2001@hotmail.com>
2023-10-17 23:40:26 -0600

    First Commit

---
+++ madlib.py
@@ -0,0 +1,45 @@
+state = input("Enter a state: ")
+boy_name = input("Enter a masculine name: ")
+animal = input("Enter an animal: ")
+body_part_1 = input("Enter a body part: ")
+body_part_2 = input("Enter another body part: ")
+feminin_name = input("Enter a feminin name: ")
+plural_noun = input("Enter a plural noun: ")
+event = input("Enter an event: ")
+verb_past = input("Enter a verb in the past tense: ")
+beverage = input("Enter a beverage: ")
+furniture = input("Enter a piece of furniture: ")
+verb = input("Enter a verb: ")
+
+madlib = f"Now somewhere in the black mountain hills of {state}\n\
+There lived a young boy named {boy_name} {animal}\n\
+And one day his woman ran off with another guy\n\
+Hit young {boy_name} in the {body_part_1}, didn't like that\n\
+He said I'm gonna get that boy\n\
+So one day he walked into town\n\
+Booked himself a room in the local saloon\n\n\
+{boy_name} {animal} checked into his room\n\
+Only to find Gideon's Bible\n\
+{boy_name} had come equipped with a gun\n\
+To shoot off the {body_part_2} of his rival\n\
+His rival it seems had broken his {plural_noun}\n\
+By stealing the girl of his fancy\n\
+Her name was Magil and she called herself Lil\n\
+But everyone knew her as {feminin_name}\n\n\
+Now she and her man who called himself Dan\n\
+Were in the next room at the {event}\n\
+{boy_name} burst in and grinning a grin\n\
+He said Danny boy this is a showdown\n\
+But Daniel was hot - he drew first and {verb_past}\n\
+And {boy_name} collapsed in the corner\n\n\
+Now the doctor came in stinking of {beverage}\n\
+And proceeded to {verb} on the {furniture}\n\
+He said {boy_name} you met your match\n\
+And {boy_name} said, doc it's only a scratch\n\
+And I'll be better, I'll be better, Doc, as soon as I am able\n\n\
+Now {boy_name} {animal} he fell back in his room\n\
+Only to find Gideon's Bible\n\
+Gideon checked out and he left it no doubt\n\
+To help with good {boy_name}'s revival"
+
+print(madlib)
